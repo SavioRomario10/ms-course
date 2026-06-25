b3d01d2317a513136d34f0ed03fa68b20056401f
Sávio Romário <savioromario10@gmail.com>
2026-06-25 15:31:15 -0300

    resolvendo questão de DNS ao subir os microserviços

--- hr-api-gateway-zuul/src/main/java/io/savioromario10/hr_api_gateway_zuul/Application.java
+++ hr-api-gateway-zuul/src/main/java/io/savioromario10/hr_api_gateway_zuul/Application.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HrApiGatewayZuulApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HrApiGatewayZuulApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
-
 }
